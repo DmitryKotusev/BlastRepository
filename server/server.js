b184accc14cca953e04ed87ed28008484da08102
Dmitry <dssa-1966@yandex.ru>
2018-04-09 22:17:35 +0300

    Second steps

--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
-var express = require('express');
-var fs = require("fs");
-var bodyParser = require('body-parser');
+const express = require('express');
+const fs = require("fs");
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+//app.use(bodyParser.json({limit: '50mb'}));
+//app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.static('../public/UI'));
+
+app.listen(3000, function () {
+    console.log('Server is running...');
+});
